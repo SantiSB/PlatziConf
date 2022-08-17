61c5b9182b7c83353bc12310ea8b964c873c63e2
santiagoprueba <santiago@gmail.com>
2022-08-17 16:41:19 -0500

    Configuración inicial del proyecto con Webpack

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+//Path
+const path = require("path");
+//Plugin HTML
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+//Elementos de la configuración
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  //Punto de entrada
+  entry: "./src/index.js",
+  //Punto de salida (donde se va a guardar la carpeta dist)
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    //Extenciones que vamos a utilizar
+    extensions: [".js", ".jsx"],
+  },
+  //Modo desarrollo
+  mode: "development",
+  module: {
+    //Reglas
+    rules: [
+      //JS y JSX
+      {
+        //Extenciones
+        test: /\.(js|jsx)$/,
+        //Excluir
+        exclude: /node_modules/,
+        //Loader a usar
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      //HTML
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      //CSS
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+        ],
+      },
+    ],
+  },
+  //Plugins
+  plugins: [
+    //Plugin HTML
+    new HtmlWebpackPlugin({
+      //Punto de entrada
+      template: "./public/index.html",
+      //Como se va a llamar en la salida
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].css",
+    }),
+  ],
+  //Servidor de trabajo local
+  devServer: {
+    //static para la ultima versión de webpack
+    static: path.join(__dirname, "dist"),
+    //Comprimir
+    compress: true,
+    //Puerto
+    port: 3005,
+  },
+};
